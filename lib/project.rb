c3b7a44ea9bfedfc1777f82a981e94876d40b8e3
Joshua Hill <josh@jlhworkshop.com>
2019-08-12 18:47:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,15 +4,15 @@ class Project
   
   def initialize(title)
     @title = title
-    @backers = []
+    @@backers = []
   end
 
   def add_backer(backer)
-    @backers << backer
+    @@backers << backer
   end
   
   def backers
-    @backers
+    @@backers
   end
   
 end
